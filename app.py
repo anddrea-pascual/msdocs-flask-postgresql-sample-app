c6937a1387f74b858d4d622cae3487cbd6ff1a8b
Sergio Alonso Zarcero <88855433+sergioalonsz@users.noreply.github.com>
2025-05-01 17:48:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -141,6 +141,9 @@ def add_imageUpload():
         image_Upload.green_pixels = int(green_pixels)
         image_Upload.blue_pixels = int(blue_pixels)
         image_Upload.username = username
+        
+        image_Upload.timestamp = datetime.strptime(request.values.get('timestamp'), "%Y-%m-%dT%H:%M:%S")
+
 
         db.session.add(image_Upload)
         db.session.commit()
